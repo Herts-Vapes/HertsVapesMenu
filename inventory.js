81291fc045f4b7087fdbac52c121b0cea258278c
Herts-Vapes <charliemanhood@hotmail.com>
2026-08-22 10:32:03 +0100

    Add files via upload

--- inventory.js
+++ inventory.js
@@ -80,8 +80,8 @@ window.HV_INVENTORY = {
   disposable: {
     title: "Disposable Vapes",
     items: [
-      { name: "Lost Mary BM6000", price: "£10", meta: "12 flavours available", popular: true, flavours: ["Cherry Cola", "Strawberry Raspberry Cherry Ice", "Banana Ice", "Double Apple", "Orange Bru", "Fizzy Cherry", "Pineapple Ice", "Lemon Lime", "Blueberry Sour Raspberry", "Strawberry Ice", "Blueberry Cherry Cranberry", "Triple Mango"] },
-      { name: "Hayati Dual Flavour 25000", price: "£15", meta: "3 flavour combinations", flavours: ["Strawberry Cranberry Cherry / Cherry Ice", "Kiwi Banana / Strawberry Banana", "Peach Mango / Cherry Ice"] },
+      { name: "Lost Mary BM6000", price: "£10", meta: "11 flavours available", popular: true, flavours: ["Strawberry Raspberry Cherry Ice", "Banana Ice", "Double Apple", "Orange Bru", "Fizzy Cherry", "Pineapple Ice", "Lemon Lime", "Blueberry Sour Raspberry", "Strawberry Ice", "Blueberry Cherry Cranberry", "Triple Mango"] },
+      { name: "Hayati Dual Flavour 25000", price: "£15", meta: "2 flavour combinations", flavours: ["Strawberry Cranberry Cherry / Cherry Ice", "Peach Mango / Cherry Ice"] },
       { name: "Enjoy Ultra 9000", price: "£10", meta: "2 flavours available", flavours: ["Berry Apple Peach", "Apple Watermelon Strawberry"] },
       { name: "Pixl 8000", price: "£10", meta: "1 flavour available", flavours: ["Sour Pineapple Mango"] },
       { name: "Hayati Pro Max 6000", price: "£10", meta: "3 flavours available", flavours: ["Blue Fusion", "Fizzy Cherry", "Mango Peach Pineapple"] },
